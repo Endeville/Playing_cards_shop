cc83d55ff66de2fa1c373c48b02b55244831e9e2
Victor <victor.popdonchev@gmail.com>
2022-04-05 11:31:23 +0300

    Initial commit with some starting html and css...

---
+++ src/main/java/bg/softuni/playing_cards_shop/PlayingCardsShopApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.playing_cards_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingCardsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayingCardsShopApplication.class, args);
+    }
+
+}
