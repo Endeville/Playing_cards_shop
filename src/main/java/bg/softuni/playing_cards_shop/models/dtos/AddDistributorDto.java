3f2b643b8891e5ad8f84ec468a0f1c3ea4a8c7b5
Victor <victor.popdonchev@gmail.com>
2022-07-29 21:17:13 +0300

    almost complete cart page - only order placement remains

--- src/main/java/bg/softuni/playing_cards_shop/models/dtos/AddDistributorDto.java
+++ src/main/java/bg/softuni/playing_cards_shop/models/dtos/AddDistributorDto.java
@@ -17,8 +17,7 @@ public class AddDistributorDto {
     private String brand;
 
     @NotBlank(message = "")
-    //todo: site url regex validation(or something else)
-//    @Pattern(regexp = "", message = "Please enter a valid url.")
+    @Pattern(regexp = "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})", message = "Please enter a valid url.")
     private String siteUrl;
 
     @NotBlank(message = "Please give a brief description of the distributor.")
